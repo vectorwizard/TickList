d990bc08c7a2e76ebc1142003b2eb88dbb6cfbed
Arijit <arijitroylochan@gmail.com>
2026-05-11 00:34:30 +0530

    Added basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Navbar from './components/navbar.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-        <div className="bg-red-600">
-          Hey I am red
+    <Navbar></Navbar>
+      <div className="container mx-auto my-5 rounded-2xl p-5 bg-violet-200 min-h-[80vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" className='bg-white color-black'/>
+          <button className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-6 hover:cursor-pointer'>Add</button>
         </div>
+          <h2 className='text-lg font-bold'>Your Todos</h2>
+          <div className="todos">
+            <div className="todo flex">
+              <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+              <div className="buttons">
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex justify-between bg-indigo-900 text-white px-5 py-2'>
+        <div className="logo">
+            <span className='font-bold text-2xl'>TickList</span>
+        </div>
+        <ul className="flex gap-8">
+            <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+        </ul>
+    </nav>
   )
 }
 
