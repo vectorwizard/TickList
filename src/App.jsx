16d41e765e3f1adacb5694d9dbfffc8a348228d6
Arijit <arijitroylochan@gmail.com>
2026-05-10 23:31:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-600">
+          Hey I am red
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
