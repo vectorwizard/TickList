cb6a3d97a2bc6a8ef3460b7f5751112b771d3a15
Arijit <arijitroylochan@gmail.com>
2026-05-11 21:22:14 +0530

    Added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Navbar from './components/navbar.jsx'
 import { v4 as uuidv4 } from "uuid";
 
@@ -6,22 +6,43 @@ import { v4 as uuidv4 } from "uuid";
 function App() {
   const [todo, settodo] = useState("")
   const [todos, settodos] = useState([])
+  
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if(todoString){
+      let todos = JSON.parse(localStorage.getItem("todos"))
+      settodos(todos)
+    }
+  }, [])
+  
+  const saveToLS = ()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }
 
-  const handleEdit = () => {
-
+  const handleEdit = (e) => {
+    let id = e.target.name;
+    let t = todos.filter(item=>item.id == id)
+    settodo(t[0].todo)
+    let newTodos = todos.filter(item => {
+      return item.id != id
+    });
+    settodos(newTodos);
+    saveToLS();
   }
 
   const handleDelete = (e) => {
     let id = e.target.name;
-    let newTodos = todos.filter(item=>{
-      return item.id!=id
+    let newTodos = todos.filter(item => {
+      return item.id != id
     });
     settodos(newTodos);
+    saveToLS();
   }
 
   const handleAdd = () => {
-    settodos([...todos, {id: uuidv4() ,todo, isCompleted: false }])
-    settodo("")
+    settodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
+    settodo("");
+    saveToLS();
   }
 
   const handleChange = (e) => {
@@ -33,7 +54,7 @@ function App() {
     let ind;
     for (let index = 0; index < todos.length; index++) {
       const element = todos[index];
-      if(element.id==id) {
+      if (element.id == id) {
         ind = index;
         break;
       }
@@ -41,6 +62,7 @@ function App() {
     let newTodos = [...todos];
     newTodos[ind].isCompleted = !newTodos[ind].isCompleted;
     settodos(newTodos);
+    saveToLS();
   }
 
   return (
@@ -50,19 +72,20 @@ function App() {
         <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
           <input name='{todo.id}' onChange={handleChange} value={todo} type="text" className='bg-white color-black w-1/2' />
-          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-6 hover:cursor-pointer'>Add</button>
+          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-6 hover:cursor-pointer'>Save</button>
         </div>
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
-          {todos.length == 0 && <div className='m-5'>No Todos to display</div> }
+          {todos.length == 0 && <div className='m-5'>No Todos to display</div>}
           {todos.map(item => {
-
             return <div key={item.id} className="todo flex w-1/2 justify-between my-3">
-              <input name={item.id} onChange={handleCheckbox} type="checkbox" value={todo.isCompleted}/>
-              <div className={item.isCompleted? "line-through": ""}>{item.todo}</div>
-              <div className="buttons">
-                <button name={item.id} onClick={handleEdit} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-1 hover:cursor-pointer'>Edit</button>
-                <button name={item.id} onClick={handleDelete} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-1 hover:cursor-pointer'>Delete</button>
+              <div className='flex gap-5'>
+                <input name={item.id} onChange={handleCheckbox} type="checkbox" value={todo.isCompleted} />
+                <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+              </div>
+              <div className="buttons flex h-full">
+                <button name={item.id} onClick={handleEdit} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-1 hover:cursor-pointer '>Edit</button>
+                <button name={item.id} onClick={handleDelete} className='bg-violet-800 hover:bg-violet-900 px-2 py-1 text-sm font-bold text-white rounded-md mx-1 hover:cursor-pointer '>Delete</button>
               </div>
             </div>
           })}
